0f7c11f48741f30ca95a05c06a4940e947d94839
vanshdeep verma <mr.vanshverma2001@gmail.com>
2025-07-23 06:30:29 +0000

    the main panel height is still not adjusted, i want you to adjust the ma

--- src/components/glass-panel-layout.tsx
+++ src/components/glass-panel-layout.tsx
@@ -284,7 +284,7 @@ const SocialButton = ({ label, href, children }: { label: string, href: string, 
 const PythonIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48" {...props}>
         <path fill="#0277BD" d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z" />
-        <path fill="#FFC107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862,5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5,1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z" />
+        <path fill="#FFC107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242-5.674-4.219c-0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862,5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5,1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z" />
     </svg>
 );
 
@@ -749,7 +749,7 @@ export function GlassPanelLayout() {
       case 'Personal':
         return (
             <div className="h-full flex flex-col gap-4">
-                <div className="bg-white/80 dark:bg-black/70 rounded-[20px] p-4 flex flex-col flex-grow min-h-0">
+                <div className="bg-white/80 dark:bg-black/70 rounded-[20px] p-4 flex flex-col flex-grow">
                     <h3 className="font-semibold text-black dark:text-white text-lg mb-2 shrink-0">Who Am I ?</h3>
                     <div className="flex-grow min-h-0">
                         <ScrollArea className="h-full pr-4">
@@ -770,7 +770,23 @@ export function GlassPanelLayout() {
                         </ScrollArea>
                     </div>
                 </div>
-                <h3 className="text-xl font-bold text-black dark:text-white mb-0 text-left shrink-0">Tools and Technologies</h3>
+                <div className="flex flex-col gap-2">
+                    <h3 className="text-xl font-bold text-black dark:text-white mb-0 text-left shrink-0">Tools and Technologies</h3>
+                     <div className="group relative w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent_0,black_20px,black_calc(100%-20px),transparent_100%)]">
+                        <div className="flex w-max animate-scroll-x gap-4 group-hover:[animation-play-state:paused]">
+                            <div className="flex shrink-0 gap-4">
+                                {technologiesWithIcons.map((tech, index) => (
+                                    <TechnologyCard key={`${tech.name}-${index}`} name={tech.name} icon={tech.icon} />
+                                ))}
+                            </div>
+                            <div className="flex shrink-0 gap-4" aria-hidden="true">
+                                {technologiesWithIcons.map((tech, index) => (
+                                    <TechnologyCard key={`${tech.name}-duplicate-${index}`} name={tech.name} icon={tech.icon} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
       case 'Career':
@@ -821,24 +837,6 @@ export function GlassPanelLayout() {
               >
                 {renderContent()}
               </GlassPanel>
-              {activeView === 'Personal' && (
-                <div className="w-[600px] mt-0">
-                    <div className="group relative w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent_0,black_20px,black_calc(100%-20px),transparent_100%)]">
-                        <div className="flex w-max animate-scroll-x gap-4 group-hover:[animation-play-state:paused]">
-                            <div className="flex shrink-0 gap-4">
-                                {technologiesWithIcons.map((tech, index) => (
-                                    <TechnologyCard key={`${tech.name}-${index}`} name={tech.name} icon={tech.icon} />
-                                ))}
-                            </div>
-                            <div className="flex shrink-0 gap-4" aria-hidden="true">
-                                {technologiesWithIcons.map((tech, index) => (
-                                    <TechnologyCard key={`${tech.name}-duplicate-${index}`} name={tech.name} icon={tech.icon} />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-              )}
             </div>
           
           <GlassPanel
