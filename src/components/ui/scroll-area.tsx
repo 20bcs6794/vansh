34e821662523247423c301431cc9a96adb91ecdc
vanshdeep verma <mr.vanshverma2001@gmail.com>
2025-08-19 16:29:33 +0000

    ake scrollbar neutral-700 for light themem and white for night theme for

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -40,7 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border opacity-70" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-neutral-700 dark:bg-white opacity-70" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
